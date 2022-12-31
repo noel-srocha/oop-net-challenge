a8745aaf0f5f002e27b8d5e502db0d7517a29429
noel-srocha <manoelrocha.n93@gmail.com>
2022-12-30 21:43:48 -0300

    OOP challenge completed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("999999999", "Nokia 3360", "sdkjhg", 64);
+Iphone iphone = new Iphone("777777777", "Iphone 3", "agfasg", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Console.WriteLine($"Número Nokia: {nokia.Numero}");
+
+nokia.InstalarAplicativo("WhatsApp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+Console.WriteLine($"Número Iphone: {iphone.Numero}");
+
+iphone.InstalarAplicativo("Procreate");
